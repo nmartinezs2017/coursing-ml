1f0937036fddaed2dbf73601ada379685b7b6d5e
Noelia Martínez Sánchez <noelia.martinez.sanchez@ericsson.com>
2021-02-15 20:58:41 +0100

    contenedores Docker de los crawlers

---
+++ courses_crawler/containers/coursera/courses_crawler/spiders/coursera.py
@@ -0,0 +1,128 @@
+from time import sleep
+
+import scrapy
+from scrapy import Selector
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import Firefox, FirefoxProfile
+from scrapy.loader import *
+from courses_crawler.items import CourseraItem
+
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+# scrapy crawl coursera -o "coursera.csv"
+def set_chrome_options():
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
+
+
+class CourseraSpider(scrapy.Spider):
+    name = "coursera"
+    start_urls = ['https://www.coursera.org/directory']
+    driver = webdriver.Chrome(options=set_chrome_options())
+
+    def parse(self, response):
+        self.driver.get(response.url)
+        absolute_page = "https://www.coursera.org"
+        sections = response.xpath('//*[@class="c-resource-button"]/@href').extract()[1:4]
+        for section in sections:
+            absolute_next_page_url = absolute_page + section
+            yield response.follow(absolute_next_page_url, self.parse_directory)
+
+    def parse_directory(self, response):
+        pages = response.xpath('//*[@class="c-directory-link"]/@href').extract()
+
+        for page in pages:
+            yield response.follow(page, self.parse_page)
+            sleep(4)
+
+    def parse_page(self, response):
+        # initialization
+        l = ItemLoader(item=CourseraItem(), response=response)
+        self.driver.get(response.url)
+        sleep(2)
+        sel = Selector(text=self.driver.page_source)
+        duration = None
+        language = None
+        difficulty = None
+        n_reviews_spe = None
+        # url
+        url = response.url
+        # section
+        section = url.split("/")[3]
+        # category
+        category = sel.xpath('//*[@class="_1ruggxy"]/a/text()').extract()
+        # number of students enrolled
+        enrolled = sel.xpath('.//strong/span/text()').extract_first()
+        # title
+        title = sel.xpath('.//h1/text()').extract_first()
+        # RATING
+        rating = sel.xpath('//*[@data-test="number-star-rating"]/text()').extract_first()
+        n_ratings = sel.xpath('//*[@class="_wmgtrl9 m-r-1s color-white"]/span/text()').extract_first()
+        n_reviews = sel.xpath('//*[@class="_bd90rg"]/*//span/text()').extract_first()
+        if section == 'specializations':
+            n_reviews_spe = sel.xpath('//*[@class="_bd90rg"]/*//span/text()').extract_first()
+        n_ratings_spe = sel.xpath('//*[@class="_wmgtrl9 color-white ratings-count-expertise-style"]/span/text()').extract_first()
+        # skills
+        skills = sel.xpath('//*[@class="_t6niqc3"]/span/@title').extract()
+        # description
+        description_spe = sel.xpath('//*[@class="description"]/*//p/text()').extract()
+        description_cou = sel.xpath('//*[@class="m-t-1 description"]/*//p/text()').extract()
+        description_pro = sel.xpath('//*[@class="_g61i7y"]/*//p/text()').extract()
+        # CHARACTERISTICS
+        # characteristics_cou = sel.xpath('//*[@class="_g61i7y"]/text()').extract()
+        # spetialization
+        characteristics_spe = sel.xpath('//*[@class="_16ni8zai m-b-0"]/text()').extract()
+        duration_week = sel.xpath('//*[@class="font-sm text-secondary"]/span/text()').extract_first()
+        if len(characteristics_spe) != 0:
+            difficulty = characteristics_spe[3]
+            duration = sel.xpath('//*[@class="_16ni8zai m-b-0"]/span/text()').extract_first()
+            language = characteristics_spe[-1]
+        # courses
+        if section == 'learn':
+            difficulty = sel.xpath('//*[@class="_16ni8zai m-b-0 m-t-1s"]/text()').extract_first()
+            duration = sel.xpath('//*[@class="_16ni8zai m-b-0 m-t-1s"]/span/text()').extract_first()
+        # LANGUAGE
+        # language_cou = sel.xpath('//*[@class="_16ni8zai m-b-0 m-t-1s"]/text()').extract()
+        # COLLABORATION
+        # professional-certificates
+        instructor = sel.xpath('//*[@class="_1qfi0x77"]/span/text()').extract_first()
+        if section == 'specializations':
+            instructor = sel.xpath('//*[@class="_1qfi0x77 instructor-count-display"]/span/text()').extract_first()
+        institution = sel.xpath('//*[@class="_1g3eaodg"]/@title').extract_first()
+
+        # add values
+        l.add_value('id_course', url.split("/")[4])
+        l.add_value('title', title)
+        l.add_value('difficulty', difficulty)
+        l.add_value('category', category)
+        l.add_value('duration', duration)
+        l.add_value('duration_week', duration_week)
+        # l.add_value('school', school)
+        l.add_value('rating', rating)
+        l.add_value('language', language)
+        l.add_value('description', description_spe)
+        l.add_value('description', description_cou)
+        l.add_value('description', description_pro)
+        l.add_value('url', response.url)
+        l.add_value('skills', skills)
+        l.add_value('enrolled', enrolled)
+        l.add_value('section', section)
+        l.add_value('n_ratings', n_ratings)
+        l.add_value('n_reviews', n_reviews)
+        l.add_value('n_reviews', n_reviews_spe)
+        l.add_value('n_ratings', n_ratings_spe)
+        l.add_value('instructor', instructor)
+        l.add_value('institution', institution)
+        l.add_value('characteristics', characteristics_spe)
+
+        return l.load_item()
---
+++ courses_crawler/containers/udacity/courses_crawler/spiders/udacity.py
@@ -0,0 +1,114 @@
+from time import sleep
+import scrapy
+from scrapy import Selector
+from selenium.common.exceptions import NoSuchElementException
+from scrapy.loader import *
+from courses_crawler.items import UdacityItem
+# from scraper_api import ScraperAPIClient
+
+# scrapy crawl udacity -o "salida.csv"
+
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+def set_chrome_options():
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
+
+
+class UdacitySpider(scrapy.Spider):
+    # client = ScraperAPIClient('b168278f17fd1cdc7040bcbdeed7c80a')
+    name = "udacity"
+    start_urls = ['https://www.udacity.com/courses/all']
+
+    def parse(self, response):
+        self.driver = webdriver.Chrome(options=set_chrome_options())
+        self.driver.get(response.url)
+        self.driver.maximize_window()
+        sleep(5)
+        self.driver.find_element_by_xpath('//*[@class="modal-close light"]').click()
+        sleep(5)
+        self.driver.find_element_by_xpath('//*[@class="button button--white acceptButton__6yUXUq"]').click()
+
+        while True:
+            sleep(5)
+            try:
+                program_details = self.driver.find_element_by_xpath('.//*[@class="card__bottom__button"]')
+                program_details.click()
+            except NoSuchElementException:
+                self.logger.info('No more pages to load.')
+                break
+
+        sel = Selector(text=self.driver.page_source)
+        cards = sel.xpath('//*[@class="catalog-cards__list__item"]') # extract
+
+        for card in cards:
+            link = card.xpath('./a/@href').extract_first() #
+            difficulty = card.xpath('//*[@data-level="beginner"]/text()').extract_first() #
+            free = card.xpath('//*[@data-type="free"]/small/text()').extract_first()
+            school = card.xpath('.//h3/text()').extract() #
+            skills = card.xpath('.//*[@class="overview__5MOH/R"]/section[1]/p/text()').extract() #
+            collaboration = card.xpath('.//*[@class="actionButtonsContainer__5MOH/R"]/div/p/text()').extract() #
+            n_reviews = card.xpath('.//*[@class="reviews__5MOH/R"]/small/text()').extract() #
+            active_stars = card.xpath('.//*[@class="active-stars"]/@style').extract() #
+
+            if (free == 'Free'):
+                is_free = True
+            else:
+                is_free = False
+
+            absolute_page = "https://www.udacity.com"
+
+            absolute_next_page_url = absolute_page + link
+            yield response.follow(absolute_next_page_url, self.parse_card, cb_kwargs=dict(difficulty=difficulty, school=school, skills=skills, is_free=is_free, collaboration=collaboration, n_reviews=n_reviews, active_stars=active_stars))
+
+    def parse_card(self, response, difficulty, school, skills, collaboration, n_reviews, active_stars, is_free):
+        l = ItemLoader(item=UdacityItem(), response=response)
+
+        # self.driver = Firefox(executable_path='geckodriver.exe')
+        # self.driver.get(response.url)
+        # sel = Selector(text=self.driver.page_source)
+        self.logger.info(response.url)
+
+        description = response.xpath('//*[@class="small hidden-md-down"]/text()').extract_first()
+        description_old = response.xpath('//*[@class="details__description"]/div/p/p/text()').extract_first()
+        description_old2 = response.xpath('//*[@class="ng-star-inserted"]/following-sibling::p/text()').extract_first()
+        duration_weeks = response.xpath('//*[@class="column-list "]/li/div/h5/text()').extract_first()
+        duration_weeks_old = response.xpath('//*[@class="details__stats"]/div/div[2]/h5/text()').extract()
+        duration_weeks_old2 = response.xpath('//*[@class="details__overview__item ng-star-inserted"]/text()').extract()
+        duration_hours = response.xpath('//*[@class="column-list "]/li/div/p/text()').extract_first()
+        # title = response.xpath('//*[@class="nd-syllabus-term__header__content"]/h2/text()').extract_first()
+        title = response.xpath('//h1/text()').extract_first()
+        # rating = sel.xpath('//*[@class="stats__average__rating"]/text()').extract_first()
+        # n_valorations = sel.xpath('//*[@class="x-small gray mb-0"]/text()').extract()
+        url:str = response.url
+        id_course = url.split('--')[1]
+
+        l.add_value('id_course', id_course)
+        l.add_value('title', title)
+        l.add_value('difficulty', difficulty)
+        l.add_value('school', school)
+        l.add_value('rating', active_stars)
+        l.add_value('n_reviews', n_reviews)
+        l.add_value('description', description)
+        l.add_value('description', description_old)
+        l.add_value('description', description_old2)
+        l.add_value('url', response.url)
+        l.add_value('skills', skills)
+        l.add_value('free', is_free)
+        l.add_value('collaboration', collaboration)
+        l.add_value('duration_weeks', duration_weeks)
+        l.add_value('duration_weeks', duration_weeks_old)
+        l.add_value('duration_weeks', duration_weeks_old2)
+        l.add_value('duration_hours', duration_hours)
+
+        return l.load_item()
