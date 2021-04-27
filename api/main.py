7f3e87071c80924e6a061fc7146d811ab6960992
Noelia Martínez Sánchez <noelia.martinez.sanchez@ericsson.com>
2021-04-28 00:13:59 +0200

    Udacity API updated

--- api/main.py
+++ api/main.py
@@ -17,8 +17,7 @@ context = load_context('../coursing-ml/')
 model_udacity = context.catalog.load("nlp_model_udacity")
 model_coursera = context.catalog.load("nlp_model_coursera")
 clustering_model_udacity = context.catalog.load("clustering_model_udacity")
-df_ud = context.catalog.load("fe_udacity")
-X_train = context.catalog.load("preprocessed_udacity")
+df_ud = context.catalog.load("cleaned_udacity")
 corpus_embeddings_udacity = context.catalog.load("corpus_embeddings_udacity")
 df_cl_ud = context.catalog.load("clustering_output_udacity")
 
@@ -34,13 +33,19 @@ class PerfilUsuario(BaseModel):
     n_reviews: int
     rating: int
 
-def inicializar_encoders():
-    X_train[['duration']].fillna("", inplace=True)
-    X_train[['rating']].fillna("", inplace=True)
-    ss.fit(X_train[['duration']])
-    mms.fit(X_train[['rating']])
 
-def convertir_datos_en_features(perfil: PerfilUsuario):
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+def inicializar_encoders_udacity():
+    df_ud[['duration']].fillna("", inplace=True)
+    df_ud[['rating']].fillna("", inplace=True)
+    ss.fit(df_ud[['duration']])
+    mms.fit(df_ud[['rating']])
+
+def convertir_datos_en_features_udacity(perfil: PerfilUsuario):
     user_difficulty = 1
     if (perfil.difficulty == 'beginner'):
         user_difficulty = 1
@@ -53,9 +58,10 @@ def convertir_datos_en_features(perfil: PerfilUsuario):
     user_popularity = 0
     user_rating = mms.transform([[perfil.rating]])
 
-    return [user_difficulty, user_duration[0][0], user_free, user_popularity, user_rating[0][0],1, 1, 1, 1, 1, 1, 1]
+    return [user_difficulty, user_duration[0][0], user_popularity, user_rating[0][0], user_free]
 
-def escoger_recomendaciones(candidatos, cluster_id, vector_usuario):
+
+def escoger_recomendaciones_udacity(candidatos, cluster_id, vector_usuario):
     resultados = dict()
     indice = 0
     for (candidato_id, score_candidato) in candidatos.items():
@@ -68,14 +74,14 @@ def escoger_recomendaciones(candidatos, cluster_id, vector_usuario):
 
     candidatos_filtrados = []
     for (candidato_id, score_candidato) in candidatos.items():
-        related_paper = df_ud.iloc[int(candidato_id)]
+        # related_paper = df_ud.iloc[int(candidato_id)]
         related_paper_features = df_cl_ud.iloc[int(candidato_id)]
         c_id_candidato = related_paper_features['Label']
         if (c_id_candidato != cluster_id):
-            rating = related_paper['rating']
+            rating = related_paper_features['rating']
             if (np.isnan(rating)):
                 rating = 0
-            vector_candidato = [related_paper['difficulty'], related_paper['duration'], related_paper['free'], related_paper['n_reviews'], rating, related_paper['school_Career Advancement'], related_paper['school_School of Artificial Intelligence'], related_paper['school_School of Autonomous Systems'], related_paper['school_School of Business'], related_paper['school_School of Cloud Computing'], related_paper['school_School of Data Science'], related_paper['school_School of Programming & Development']]
+            vector_candidato = [related_paper_features['difficulty'], related_paper_features['duration'], related_paper_features['n_reviews'], rating, related_paper_features['free']]
             cos_sim = dot(vector_candidato, vector_usuario)/(norm(vector_candidato)*norm(vector_usuario))
             candidatos_filtrados.append((candidato_id, cos_sim))
 
@@ -98,42 +104,21 @@ def search_courses_udacity(title, description = ""):
     search_hits = search_hits[0]  #Get the hits for the first query
 
     results = dict()
-    print("\n\nCourse:", title)
-    print("Most similar course:")
     for hit in search_hits:
         related_paper = df_ud.iloc[hit['corpus_id']]
-        results[str(hit['corpus_id'])] = float(hit['score'])
-        print("{:.2f}\t{}".format(hit['score'], related_paper['title']))
+        results[str(hit['corpus_id'])] = { "score": float(hit['score']), "title": related_paper['title'], "url": related_paper["url"] }
 
     return dict(results)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/search_courses/")
-def semantic_search(title: str, description: Optional[str] = ""):
+@app.get("/search_courses_udacity/")
+def semantic_search_udacity(title: str, description: Optional[str] = ""):
     list_courses_udacity = search_courses_udacity(title, description)
     return list_courses_udacity
 
-@app.get("/clustering/course-cluster/{id_course}")
-def get_course_cluster(id_course: int):
-    return {'id_course': id_course, 'clustering_id': df_cl_ud.iloc[id_course]['Label']}
 
-@app.get("/clustering/courses-list/{id_cluster}")
-def get_list_cluster(id_cluster: int):
-    list_courses = df_cl_ud[df_cl_ud['Label']==id_cluster].index.tolist()
-    return {'clustering_id': id_cluster, 'list_courses': list_courses}
-
-@app.get("/clustering/clusters-list/")
-def get_list_cluster():
-    list_courses = df_cl_ud['Label'].unique().tolist()
-    return {'list_courses': list_courses}
-
-
-@app.post("/recommend/")
-def recommendation(perfil: PerfilUsuario):
+@app.post("/recommend_udacity/")
+def recommendation_udacity(perfil: PerfilUsuario):
     query_embedding = model_udacity.encode(perfil.description, convert_to_tensor=True)
 
     search_hits = util.semantic_search(query_embedding, corpus_embeddings_udacity, top_k=30)
@@ -144,14 +129,30 @@ def recommendation(perfil: PerfilUsuario):
         # related_paper = df_ud.iloc[hit['corpus_id']]
         results[str(hit['corpus_id'])] = float(hit['score'])
 
-    row_user = convertir_datos_en_features(perfil)
-    df_user = pd.DataFrame([row_user], columns=["difficulty","duration","free","n_reviews","rating","school_Career Advancement","school_School of Artificial Intelligence","school_School of Autonomous Systems","school_School of Business","school_School of Cloud Computing","school_School of Data Science","school_School of Programming & Development"])
+    row_user = convertir_datos_en_features_udacity(perfil)
+    df_user = pd.DataFrame([row_user], columns=["difficulty","duration","n_reviews", "rating", "free"])
+    print(df_user)
 
     labels = clustering_model_udacity.predict(df_user)
     cluster_id = labels[0]
-    list_recommendations = escoger_recomendaciones(results, cluster_id, row_user)
+    print(cluster_id)
+    print(results)
+    list_recommendations = escoger_recomendaciones_udacity(results, cluster_id, row_user)
 
     return {'list_recommendations': list_recommendations}
 
+# @app.get("/clustering/course-cluster/{id_course}")
+# def get_course_cluster(id_course: int):
+#    return {'id_course': id_course, 'clustering_id': df_cl_ud.iloc[id_course]['Label']}
 
-inicializar_encoders()
+# @app.get("/clustering/courses-list/{id_cluster}")
+# def get_list_cluster(id_cluster: int):
+#    list_courses = df_cl_ud[df_cl_ud['Label']==id_cluster].index.tolist()
+#    return {'clustering_id': id_cluster, 'list_courses': list_courses}
+
+# @app.get("/clustering/clusters-list/")
+# def get_list_cluster():
+#     list_courses = df_cl_ud['Label'].unique().tolist()
+#     return {'list_courses': list_courses}
+
+inicializar_encoders_udacity()
